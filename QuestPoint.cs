d1d01abac0c29705a77e9ccffbc05416f43959c1
Jutta Mujunen <AB5893@student.jamk.fi>
2024-01-11 16:56:50 +0200

    HitW Updates

---
+++ QuestPoint.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class QuestPoint : MonoBehaviour
+{
+    [Header("Quest")]
+    [SerializeField] private QuestScriptableObject questInfoForPoint;
+
+    [Header("Config")]
+    [SerializeField] private bool startPoint = true;
+    [SerializeField] private bool finishPoint = true;
+
+    [Header("Dialogue Config")]
+    [SerializeField] private bool hasDialogue;
+    [SerializeField] private TextAsset startQuestDialogue;
+    [SerializeField] private TextAsset finishQuestDialogue;
+
+    [Header("Dialogue Config")]
+    [SerializeField] AudioSource dialogueSound;
+    [SerializeField]private bool playerIsNear = false;
+    private string questId;
+    private QuestState currentQuestState;
+
+    private void Awake()
+    {
+        questId = questInfoForPoint.id;
+    }
+
+    private void OnEnable()
+    {
+        GameEventsManager.instance.questEvents.onQuestStateChange += QuestStateChange;
+    }
+
+    private void OnDisable()
+    {
+        GameEventsManager.instance.questEvents.onQuestStateChange -= QuestStateChange;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            InteractedWithQuestPoint();
+        }
+    }
+
+    private void InteractedWithQuestPoint()
+    {
+        if (!playerIsNear)
+        {
+            return;
+        }
+
+        // start or finish a quest
+        if(currentQuestState.Equals(QuestState.CAN_START) && startPoint)
+        {
+            if(hasDialogue && startQuestDialogue != null)
+            {
+                DialogueManager.instance.StartDialogue(startQuestDialogue);
+                dialogueSound.Play();
+            }
+
+            GameEventsManager.instance.questEvents.StartQuest(questId);
+        }
+
+        else if(currentQuestState.Equals(QuestState.CAN_FINISH) && finishPoint)
+        {
+            if (hasDialogue && finishQuestDialogue != null)
+            {
+                DialogueManager.instance.StartDialogue(finishQuestDialogue);
+            }
+
+            GameEventsManager.instance.questEvents.FinishQuest(questId);
+        }
+    }
+
+    private void QuestStateChange(Quest quest)
+    {
+        // only update the quest state if this point has the corresponding quest
+        if (quest.info.id.Equals(questId))
+        {
+            currentQuestState = quest.state;
+            Debug.Log("Quest with id: " + questId + " updated to state: " + currentQuestState);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsNear = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsNear = false;
+        }
+    }
+}
